6238e8f70b6081b5b83dbcc6723d352e5b4ece43
Oğuz Adıgüzel <oguz_adiguzel@outlook.com>
2023-03-30 23:54:02 +0300

    new commit

--- pages/login.js
+++ pages/login.js
@@ -20,8 +20,8 @@ function Login() {
             <div className="w-full md:w-1/2 h-96 bg-afterpay-orange mt-16 flex justify-center flex-col items-center">
                 <h2 className="text-3xl mb-4 font-bold text-white">Kullanıcı Girişi</h2>
                 <form className="flex flex-col">
-                    <label htmlFor="userName" className="text-white text-xl">Kullanıcı Adı</label>
-                    <input value={email} onChange={(e) => dispatch(handleChangeUser(e))} id="userName" type='text' placeholder="Kullanıcı Adınızı Giriniz" className="w-64 h-9 pl-2 rounded-md" />
+                    <label htmlFor="userName" className="text-white text-xl">E-mail</label>
+                    <input value={email} onChange={(e) => dispatch(handleChangeUser(e))} id="userName" type='text' placeholder="E-mail Giriniz" className="w-64 h-9 pl-2 rounded-md" />
                     <label htmlFor="password" name="password" className="text-white text-xl mt-5">Parola</label>
                     <input value={password} onChange={(e) => dispatch(handleChangePassword(e))} id="password" type='password' placeholder="Parola Giriniz" className="w-64 h-9 pl-2 rounded-md" />
                     <button onClick={(e) => {
